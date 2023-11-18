a0430bf3c5bed85133b0aba75934904bb4a5afda
Karol <malickikarol0@gmail.com>
2023-11-18 14:00:19 +0100

    Added default layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { HomeComponent } from './modules/home/home.component';
+import { ProductComponent } from './modules/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'products',
+        component: ProductComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/shared/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../components/footer/footer.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { SidebarComponent } from '../components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { FlexModule } from '@angular/flex-layout';
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent, SidebarComponent],
+  imports: [CommonModule, RouterModule, FlexModule],
+  exports: [HeaderComponent, FooterComponent, SidebarComponent],
+})
+export class SharedModule {}
