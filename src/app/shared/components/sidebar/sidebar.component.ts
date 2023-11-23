fbe6f4be36fcfc4641e7a6e8fcad151726318f8b
Karol <malickikarol0@gmail.com>
2023-11-23 09:11:06 +0100

    Made basic styles

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  categories: string[] = ['Category 1', 'Category 2', 'Category 3'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,11 +5,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 import { FlexModule } from '@angular/flex-layout';
-import { MatIconModule } from '@angular/material/icon';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, SidebarComponent],
-  imports: [CommonModule, RouterModule, FlexModule, MatIconModule],
+  imports: [CommonModule, RouterModule, FlexModule, MaterialModule],
   exports: [HeaderComponent, FooterComponent, SidebarComponent],
 })
 export class SharedModule {}
